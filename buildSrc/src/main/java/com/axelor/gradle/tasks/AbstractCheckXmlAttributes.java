3c4eb22a953ed9dba22e3049323b6159a76f900a
phongTG-98 <phong@newl.io>
2024-09-07 15:16:51 +0700

    update errorlist

--- buildSrc/src/main/java/com/axelor/gradle/tasks/AbstractCheckXmlAttributes.java
+++ buildSrc/src/main/java/com/axelor/gradle/tasks/AbstractCheckXmlAttributes.java
@@ -90,7 +90,7 @@ public abstract class AbstractCheckXmlAttributes extends DefaultTask {
 
   protected void addErrorsProperlyToErrorList(List<String> errorList, @javax.annotation.Nonnull Exception error) {
     if (!error.getLocalizedMessage().startsWith("cvc-elt.1.a")) {
-      errorList.add(List.of(error.toString()));
+      errorList.addAll(List.of(error.toString()));
     }
   }
 }
