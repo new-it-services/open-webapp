2d7fad85f87c406d6d99aeb97b1b77825fd6968f
phongTG-98 <phong@newl.io>
2024-09-07 17:04:04 +0700

    update errorlist

--- src/main/webapp/map/js/Control.Geocoder.js
+++ src/main/webapp/map/js/Control.Geocoder.js
@@ -1,12 +1,21 @@
-/* @preserve
- * Leaflet Control Geocoder 1.7.0
- * https://github.com/perliedman/leaflet-control-geocoder
+/*
+ * Axelor Business Solutions
  *
- * Copyright (c) 2012 sa3m (https://github.com/sa3m)
- * Copyright (c) 2018 Per Liedman
- * All rights reserved.
+ * Copyright (C) 2005-2024 Axelor (<http://axelor.com>).
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  */
-
 this.L = this.L || {};
 this.L.Control = this.L.Control || {};
 this.L.Control.Geocoder = (function (L) {
