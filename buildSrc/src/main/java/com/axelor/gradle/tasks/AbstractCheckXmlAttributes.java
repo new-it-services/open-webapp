5f104867c6c3befddb6af10e8b6c50e15eac6ee7
phongTG-98 <phong@newl.io>
2024-09-07 11:50:37 +0700

    Update AbstractCheckXmlAttributes.java

--- buildSrc/src/main/java/com/axelor/gradle/tasks/AbstractCheckXmlAttributes.java
+++ buildSrc/src/main/java/com/axelor/gradle/tasks/AbstractCheckXmlAttributes.java
@@ -88,7 +88,7 @@ public abstract class AbstractCheckXmlAttributes extends DefaultTask {
 
   protected void addErrorsProperlyToErrorList(List<String> errorList, Exception error) {
     if (!error.getLocalizedMessage().startsWith("cvc-elt.1.a")) {
-      errorList.addAll(List.of(error.toString[].split(";")));
+      errorList.addAll(List.of(error.toString().split(";")));
     }
   }
 }
